ccabf37c52602e9615d6f2ec3f92ef8836e6d5f2
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-18 13:06:31 +0100

    Add Components to Pages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,21 @@
 import "./Home.css"
+import { Header } from "../../components/Header/Header"
+import { BurgerMenu } from "../../components/BurgerMenu/BurgerMenu"
+import { SearchBar } from "../../components/SearchBar/SearchBar"
+import { DarkMode } from "../../components/DarkMode/DarkMode"
+import { Popup } from "../../components/Popup/Popup"
 
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <main>
+      <h1>Home</h1>
+      <Header />
+      <div>
+        <BurgerMenu />
+        <SearchBar />
+        <DarkMode />
+      </div>
+      <Popup />
+    </main>
+  )
 }
