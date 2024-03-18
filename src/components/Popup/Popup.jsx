7c98da715e89331054a68cdb87b55f88e808ffc4
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-18 14:05:34 +0100

    Fetch Type Data for Popup

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,5 +1,33 @@
 import "./Popup.css"
+import { useState, useEffect } from "react"
 
 export const Popup = () => {
-  return <div>Popup</div>
+  const [type, setType] = useState([])
+  console.log(type)
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/type/")
+      .then((res) => res.json())
+      .then((data) => setType(data.results))
+  }, [])
+
+  return (
+    <main>
+      <section className='popup'>
+        <h1 className='popup__heading'>Type</h1>
+        <form className='popup__form'>
+          <div className='popup__grid'>
+            {type.map((item, index) => {
+              return (
+                <p className='popup__grid__items' key={index}>
+                  {item.name !== "unknown" && item.name}
+                </p>
+              )
+            })}
+          </div>
+          <input type='submit' value='Search' />
+        </form>
+      </section>
+    </main>
+  )
 }
