c2ea0fe6b784b551d4aa32aab2ac40c2c0336d14
Nostea <kiwiuchi@googlemail.com>
2024-03-18 17:11:23 +0100

    added functionality and styling of the pokemon card in the Home page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,30 @@
-import "./Card.css"
+import { Link } from "react-router-dom";
+import "./Card.css";
+import { useEffect, useState } from "react";
 
-export const Card = () => {
-  return <div>Card</div>
-}
+export const Card = (props) => {
+  const [pokemonData, setPokemonData] = useState({});
+  useEffect(() => {
+    fetch(props.imgURL)
+      .then((res) => res.json())
+      .then((apiData) => setPokemonData(apiData))
+      .catch((error) => console.error("Error in Card.jsx fetch", error));
+  }, []);
+  console.log(pokemonData);
+
+  return (
+    <article className="card__container">
+      {pokemonData ? (
+        <div>
+          <img src={pokemonData?.sprites?.other?.home?.front_default} alt="" />
+          <div className="card__details">
+            <p>{pokemonData?.id}</p>
+            <p className="card__details__title">{pokemonData?.name}</p>
+          </div>
+        </div>
+      ) : (
+        <p>loading</p>
+      )}
+    </article>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
-import "./Home.css"
-import { Header } from "../../components/Header/Header"
-import { BurgerMenu } from "../../components/BurgerMenu/BurgerMenu"
-import { SearchBar } from "../../components/SearchBar/SearchBar"
-import { DarkMode } from "../../components/DarkMode/DarkMode"
-import { Popup } from "../../components/Popup/Popup"
+import "./Home.css";
+import { Header } from "../../components/Header/Header";
+import { BurgerMenu } from "../../components/BurgerMenu/BurgerMenu";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import { DarkMode } from "../../components/DarkMode/DarkMode";
+import { Popup } from "../../components/Popup/Popup";
+import { Card } from "../../components/Card/Card";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+  // 1025 einträge, der Rest sind spezielle Formen von pokemon
+  // https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1025
+  //izel die lösung für die ladezeiten mal schauen
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((apiData) => setPokemonData(apiData))
+      .catch((error) => console.error("Error in Home.jsx fetch", error));
+  }, []);
+  console.log(pokemonData);
+
   return (
     <main>
       <h1>Home</h1>
@@ -15,7 +29,11 @@ export const Home = () => {
         <SearchBar />
         <DarkMode />
       </div>
+
+      <section className="home__cardContainer">
+        {pokemonData.results ? pokemonData?.results.map((item, index) => <Card imgURL={item?.url} key={index} title={item.name} />) : <p>loading...</p>}
+      </section>
       <Popup />
     </main>
-  )
-}
+  );
+};
