78825fa6cc485d7017b49b91de08ad0c55f07cfc
ChrissQAng <goodomen@outlook.de>
2024-03-19 10:31:12 +0100

    search bar implemented in detail and color types

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -4,14 +4,15 @@ import { Header } from "../../components/Header/Header";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 import { DarkMode } from "../../components/DarkMode/DarkMode";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { colors } from "../../assets/data/data";
 
 export const Details = () => {
   //  --- useState
   const [singlePokemon, setSinglePokemon] = useState([]);
   // --- store link id
   const { id } = useParams();
-  const testId = 1;
+
   // --- fetch
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${Number(id)}`)
@@ -27,7 +28,25 @@ export const Details = () => {
 
       {singlePokemon.sprites ? (
         <div className="detail">
-          <div className="detail__image-headline">
+          <div className="detail__searchbar">
+            <Link to="/">
+              <svg
+                width="18"
+                height="28"
+                viewBox="0 0 18 28"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16.2195 26.4203C17.1914 25.4867 17.1922 23.9324 16.2214 22.9978L9.86802 16.8817C8.23349 15.3082 8.23349 12.6918 9.86802 11.1183L16.2214 5.00221C17.1922 4.06762 17.1914 2.51326 16.2195 1.57973C15.3007 0.697176 13.8491 0.697176 12.9303 1.57973L3.00321 11.1153C1.36449 12.6893 1.36449 15.3107 3.00321 16.8847L12.9303 26.4203C13.8491 27.3028 15.3007 27.3028 16.2195 26.4203Z"
+                  fill="#EBEBEB"
+                />
+              </svg>
+            </Link>
+            <SearchBar />
+            <DarkMode />
+          </div>
+          <div className="detail__image-title">
             <div className="detail__image_wrapper">
               <img
                 src={singlePokemon.sprites.other.home.front_default}
@@ -40,7 +59,14 @@ export const Details = () => {
           </div>
           <div className="detail__types">
             {singlePokemon.types.map((item, index) => (
-              <p key={index}>{item.type.name}</p>
+              <p
+                key={index}
+                style={{
+                  backgroundColor: colors[item.type.name],
+                }}
+              >
+                {item.type.name}
+              </p>
             ))}
           </div>
         </div>
