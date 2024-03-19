4269d65ace63b376c3f3ee8f696cf377346eeb24
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-19 08:03:13 +0100

    Add Search Bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react"
 import "./SearchBar.css"
 
 export const SearchBar = () => {
-  return <div>SearchBar</div>
+  // # State in Home Page und über Props abfragen
+  const [searchResult, setSearchResult] = useState("")
+
+  return (
+    <form className='search' onSubmit={(e) => e.preventDefault()}>
+      <input
+        className='search__input'
+        type='text'
+        name='search'
+        id='search'
+        placeholder='Search Pokemon'
+        value={searchResult}
+        onChange={(e) => setSearchResult(e.target.value)}
+      />
+    </form>
+  )
 }
