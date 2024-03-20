3764241ba5bbeac6e4261c5e6ac8debb1314bf4e
ChrissQAng <goodomen@outlook.de>
2024-03-20 10:11:29 +0100

    add dark mode

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -3,17 +3,20 @@ import "../Details/Details";
 import { Header } from "../../components/Header/Header";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 import { DarkMode } from "../../components/DarkMode/DarkMode";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { colors } from "../../assets/data/data";
+import { darkModeContext } from "../../context/Context";
 
 export const Details = () => {
   //  --- useState
   const [singlePokemon, setSinglePokemon] = useState([]);
-  // search function usestates
+  // ----------- search function usestates
   const [searchText, setSearchText] = useState("");
   const [pokemonByName, setPokemonByName] = useState(null);
   const [pokemonFilteredData, setPokemonFilteredData] = useState([]);
+  // --- useContext
+  const { darkMode } = useContext(darkModeContext);
 
   // --- store link id
   const { id } = useParams();
@@ -47,7 +50,7 @@ export const Details = () => {
       <Header />
 
       {singlePokemon.sprites ? (
-        <div className="detail">
+        <div className={darkMode ? "detail dark" : "detail"}>
           <div className="detail__searchbar">
             <Link to="/">
               <svg
@@ -55,7 +58,8 @@ export const Details = () => {
                 height="28"
                 viewBox="0 0 18 28"
                 fill="none"
-                xmlns="http://www.w3.org/2000/svg">
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path
                   d="M16.2195 26.4203C17.1914 25.4867 17.1922 23.9324 16.2214 22.9978L9.86802 16.8817C8.23349 15.3082 8.23349 12.6918 9.86802 11.1183L16.2214 5.00221C17.1922 4.06762 17.1914 2.51326 16.2195 1.57973C15.3007 0.697176 13.8491 0.697176 12.9303 1.57973L3.00321 11.1153C1.36449 12.6893 1.36449 15.3107 3.00321 16.8847L12.9303 26.4203C13.8491 27.3028 15.3007 27.3028 16.2195 26.4203Z"
                   fill="#EBEBEB"
@@ -66,6 +70,7 @@ export const Details = () => {
               setSearchResult={setSearchText}
               searchResult={searchText}
             />
+            {/* --- article for search pop-up */}
             <article className="detail__serch-options"></article>
             <DarkMode />
           </div>
@@ -88,8 +93,9 @@ export const Details = () => {
               <p
                 key={index}
                 style={{
-                  backgroundColor: colors[item.type.name]
-                }}>
+                  backgroundColor: colors[item.type.name],
+                }}
+              >
                 {item.type.name}
               </p>
             ))}
